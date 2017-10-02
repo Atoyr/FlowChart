169b86ce039d20b2394e88282845fd08bc4d07b1
r.uchiyama <r.uchiyama@sint.co.jp>
2017-10-02 13:04:47 +0900

    Modeをjsからpropertyに変更

--- src/editor.js
+++ src/editor.js
@@ -1,6 +1,6 @@
 function onLoad() {
     editor = ace.edit("editor");
-    editor.getSession().setMode("ace/mode/javascript");
+    editor.getSession().setMode("ace/mode/properties");
     editor.setTheme("ace/theme/twilight");
  }
 
